df5e9b5b894e698beb0aadcd6eab05b7701ca8a4
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2025-02-26 15:20:50 +0530

    initial commit

---
+++ fake-rest-service/src/main/java/com/web/fake/FakeRestApplication.java
@@ -0,0 +1,13 @@
+package com.web.fake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeRestApplication.class, args);
+	}
+
+}
---
+++ fake-rest-service/src/test/java/com/web/fake/FakeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.fake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
